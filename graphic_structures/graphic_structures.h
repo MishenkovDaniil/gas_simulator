e5fef5b39c02f6d8afbeea3220f01b476ee78028
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-09-20 01:53:38 +0300

    delete unused part and refactored class button (now in button/button.h)

--- graphic_structures/graphic_structures.h
+++ graphic_structures/graphic_structures.h
@@ -46,84 +46,13 @@ public:
     double z_ = 0;
 
 public:
-    Point (double x, double y, double z) : x_ (x), y_ (y), z_ (z) {};
-    Point (double x, double y) : x_ (x), y_ (y), z_ (0) {};
+    Point (double x = 0, double y = 0, double z = 0) : x_ (x), y_ (y), z_ (z) {};
+    // Point (double x, double y) : x_ (x), y_ (y), z_ (0) {};
     Point (const Point &point) : x_ (point.x_), y_ (point.y_), z_ (point.z_) {};
 
     void update (double x, double y, double z);
+    double distance (Point &point) {return sqrt((x_ - point.x_) * (x_ - point.x_) + (y_ - point.y_) * (y_ - point.y_) + (z_ - point.z_) * (z_ - point.z_));}
 };
-
-
-//virtual function draw: one for drawing picture inside button, one for text button,etc.
-//button manager: array of pointers to buttons, checks if some button is pressed, cause action for pressed button,, draws all buttons at once 
-class Button 
-{
-protected:
-    Point lh_corner_ = Point (0, 0, 0); /// z coord is not used!!!
-    Point rl_corner_ = Point (0, 0, 0); /// z coord is not used!!!
-    
-    Color color_                 = Color ();
-    Color pressed_button_color   = Color (0, 0xff, 0, 0xff);
-    Color unpressed_button_color = Color ();
-    
-    int  button_use_ = NONE; //
-    bool is_pressed_ = false;
-
-    char *string_ = nullptr;
-    int str_size = 0;
-
-public:
-    Button () {;};
-    Button (Point lh_corner, Point rl_corner, Color color, const char *string, const int button_use) :
-                                                        lh_corner_             (lh_corner), 
-                                                        rl_corner_             (rl_corner),
-                                                        button_use_            (button_use),//
-                                                        color_                 (color),
-                                                        unpressed_button_color (color),
-                                                        is_pressed_            (false)
-                                                        
-                                                        {str_size = sizeof(string);
-                                                         string_ = new char[str_size + 1];
-                                                         assert (string_);
-                                                         std::strcpy (string_, string); 
-                                                        };
-    ~Button () {if (string_) delete[] string_;};
-
-    void draw (sf::RenderWindow &window, int screen_h, int screen_w)const;
-    void update (bool is_pressed);
-    bool get_status () const {return is_pressed_;};
-    bool contains (double x, double y) const;
-    int get_button_use () const {return button_use_;};
-    // virtual void run (Point &object, sf::Keyboard::Key key);
-};
-
-// class Move_button : public Button 
-// {
-//     Point object_ = Point (0, 0, 0);
-// public:
-//     Move_button (Point &object, Point lh_corner, Point rl_corner, Color color, const char *string, const int button_use) :
-//                                                         object_ (object),
-//                                                         Button (lh_corner, rl_corner, color, string, button_use)
-//                                                         {;};
-// };
-
-class Button_manager 
-{
-    Button **buttons_p = nullptr;
-    int size = 0;
-    int capacity = 0;
-
-public: 
-    Button_manager ();
-    ~Button_manager ();
-
-    void add (Button *button);
-    void draw ();
-    bool run ();
-private:
-    bool resize ();
-};
-
 
 class Vector 
 {
@@ -133,7 +62,7 @@ public://
     double z_ = 0;
 
 public:
-    Vector (double x, double y, double z) : x_ (x), y_ (y), z_ (z) {};
+    Vector (double x = 0, double y = 0, double z = 0) : x_ (x), y_ (y), z_ (z) {};
     Vector (const Point &start, const Point &end) : x_ (end.x_ - start.x_),
                                                     y_ (end.y_ - start.y_), 
                                                     z_ (end.z_ - start.z_) {};
