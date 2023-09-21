6d7742363d882c319c28cbb6d3499bec9a7526f2
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-09-22 00:26:44 +0300

    add piston constants, add temp button

--- main.cpp
+++ main.cpp
@@ -2,49 +2,79 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
-// #include "mol/mol.h"
-// #include "graphic_structures/graphic_structures.h"
-// #include "environment/environment.h"
+#include "mol/mol.h"
+#include "graphic_structures/graphic_structures.h"
+#include "environment/environment.h"
 #include "buttons/buttons.h"
 
 static const int WINDOW_WIDTH = 800;
 static const int WINDOW_HEIGHT = 800;
+static const int DELTA_BUTTON_HEIGHT = 50;
+static const int DELTA_BUTTON_WIDTH = 100;
+
+static const Color PURPLE = Color (255, 0, 255, 255);
+static const Color GREEN  = Color (0, 255, 0, 255);
+static const Color RED    = Color (255, 0, 0, 255);
+static const Color BLUE   = Color (0, 0, 255, 255);
+
+static const int PISTON_THICKNESS = 40;
+static const int PISTON_HEIGHT = 100;
 
 int main ()
 {
-    sf::RenderWindow window (sf::VideoMode (WINDOW_WIDTH, WINDOW_HEIGHT), "gas_exp");
+    sf::RenderWindow window (sf::VideoMode (WINDOW_WIDTH, WINDOW_HEIGHT), "gas_exp", sf::Style::Fullscreen); //??The requested video mode is not available, switching to a valid mode
 
     sf::RenderTexture texture;
     texture.create (WINDOW_WIDTH, WINDOW_HEIGHT);
-    
-    Mol_manager mol_manager (texture);
-    mol_manager.create (10, ROUND_MOL);
-    mol_manager.create (10, SQUARE_MOL);
     
     Point ll (0, WINDOW_HEIGHT); 
     Point lh (0, 0);
     Point rl (WINDOW_WIDTH, WINDOW_HEIGHT);
     Point rh (WINDOW_WIDTH, 0);
 
+    Point piston_lh (0, PISTON_HEIGHT);
+    Point piston_rl (WINDOW_WIDTH, PISTON_HEIGHT);
+
     Wall low (ll, rl, texture);
     Wall left (lh, ll, texture);
     Wall right (rh, rl, texture);
-    Piston piston (lh, rh, texture);
+    // Piston piston (lh, rh, texture);
+    Piston piston (piston_lh, piston_rl, texture, PISTON_THICKNESS);
+    
+    Mol_manager mol_manager (texture, WINDOW_WIDTH, WINDOW_HEIGHT, piston_lh);
 
     mol_manager.add_wall (&low);
     mol_manager.add_wall (&right);
     mol_manager.add_wall (&left);
     mol_manager.add_wall (&piston);
+
+    mol_manager.create (10, ROUND_MOL);
+    mol_manager.create (10, SQUARE_MOL);
     
     texture.display ();
     Button_manager button_manager (window, WINDOW_WIDTH, WINDOW_HEIGHT);
 
-    Piston_button piston_button (piston, Point (0, 0), Point (100, 50), Color (255, 0, 255, 255), "piston", NONE, Color (0, 255, 0, 255));
-    Square_mol_button square_mol_button (mol_manager, Point (0, 50), Point (100, 100), Color (255, 0, 255, 255), "add square", NONE);
-    Round_mol_button round_mol_button (mol_manager, Point (0, 100), Point (100, 150), Color (255, 0, 255, 255), "add round", NONE);
+    int button_start_x = 0;
+
+    Piston_button piston_button         (mol_manager, Point (button_start_x, WINDOW_HEIGHT), 
+                                                      Point (100, WINDOW_HEIGHT + DELTA_BUTTON_HEIGHT),
+                                                      PURPLE, "PISTON", NONE, GREEN);
+                                                      ///button_start_x = 1 * DELTA_BUTTON_WIDTH
+    Square_mol_button square_mol_button (mol_manager, Point (100, WINDOW_HEIGHT), 
+                                                      Point (200, WINDOW_HEIGHT + DELTA_BUTTON_HEIGHT),
+                                                      PURPLE, "ADD SQUARE", NONE);
+                                                      ///button_start_x = 2 * DELTA_BUTTON_WIDTH
+    Round_mol_button round_mol_button   (mol_manager, Point (200, WINDOW_HEIGHT),
+                                                      Point (300, WINDOW_HEIGHT + DELTA_BUTTON_HEIGHT),
+                                                      PURPLE, "ADD ROUND", NONE);
+                                                      ///button_start_x = 3 * DELTA_BUTTON_WIDTH
+    Temp_button      temp_button        (mol_manager, Point (300, WINDOW_HEIGHT),
+                                                      Point (400, WINDOW_HEIGHT + DELTA_BUTTON_HEIGHT),
+                                                      PURPLE, "TEMP", NONE, GREEN);
     button_manager.add (&piston_button);
     button_manager.add (&square_mol_button);
     button_manager.add (&round_mol_button);
+    button_manager.add (&temp_button);
 
     sf::Sprite sprite;
 
@@ -63,7 +93,7 @@ int main ()
                      (event.mouseButton.x || event.mouseButton.y))
             {
                 Button *pressed_button = button_manager.contains(event.mouseButton.x, event.mouseButton.y);
-
+                fprintf (stderr, "[%p]\n", pressed_button);
                 if (pressed_button)
                 {   
                     pressed_button->update (!(pressed_button->get_status ()));
@@ -72,7 +102,7 @@ int main ()
             }
             else if (event.type == sf::Event::KeyPressed)
             {
-                button_manager.run (Point (0, 0, 0), event.key.code);
+                button_manager.run (Point (), event.key.code);
             }
         }
 
@@ -87,6 +117,9 @@ int main ()
         window.display();
 
         mol_manager.move ();
+        // mol_manager.update_height (-10);
+
+        // mol_manager.update_temperature (mol_manager.get_temperature () + 1);
     }
     
     return 0;
