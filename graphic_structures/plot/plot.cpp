ec17d2ea77a467c0f8b2be3be7b372b7982adfab
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-09-27 01:56:57 +0300

    add max_value, fixed bug with not initializing coord_system, made ordinate coord calc properly

--- graphic_structures/plot/plot.cpp
+++ graphic_structures/plot/plot.cpp
@@ -1,12 +1,13 @@
 #include "plot.h"
 #include <cmath>
 
-Plot::Plot (Point &start, int x_len, int y_len, Color color) 
+Plot::Plot (Point &start, int x_len, int y_len, double max_val, Color color) 
           : size_ (x_len),
             start_ (start), 
             color_ (color),
             ordinate_len_ (y_len),
-            abscissa_len_ (x_len)
+            abscissa_len_ (x_len),
+            max_val_ (max_val)
 {
     if (size_ <= 0)
     {
@@ -14,6 +15,8 @@ Plot::Plot (Point &start, int x_len, int y_len, Color color)
         return;
     }
     coord_system = Coord_system (start_.x_, start_.y_, abscissa_len_, ordinate_len_, 0, 0, sf::VideoMode (abscissa_len_, ordinate_len_));
+    
+    array = new sf::VertexArray (sf::Points, size_);
 
     points_ = new Point[size_];
     assert (points_);
@@ -23,20 +26,21 @@ Plot::~Plot ()
 {
     size_ = INT64_MIN;
     delete[] points_;
+    delete array;
     points_ = nullptr;
 }
 
 void Plot::draw (sf::RenderTarget &target, double param)
 {
-    static sf::VertexArray array (sf::Points, size_);
-    
+    // static sf::VertexArray array (sf::Points, size_);
+    double coeff = max_val_ / ordinate_len_;
     if (cur == size_)
         is_filled = true;
     if (!is_filled)
     {
-        array[cur].position = sf::Vector2f (start_.x_  + cur, start_.y_ - param);
-        array[cur].color = sf::Color::Red;
-        points_[cur] = Point (start_.x_ + cur, start_.y_ + param);
+        (*array)[cur].position = sf::Vector2f (start_.x_  + cur, start_.y_ - param / coeff);
+        (*array)[cur].color = sf::Color::Red;
+        points_[cur] = Point (start_.x_ + cur, start_.y_ - param / coeff);
         cur++;
     }
     else
@@ -62,12 +66,12 @@ void Plot::draw (sf::RenderTarget &target, double param)
 
         for (int i = 0; i < size_ - 1; ++i)
         {
-            array[i].position.y = points_[i + 1].y_;
+            (*array)[i].position.y = points_[i + 1].y_;
             points_[i] = points_[i + 1];
-            array[i].color = sf::Color::Red;
+            (*array)[i].color = sf::Color::Red;
         }
-        points_[size_ - 1] = Point (start_.x_ + size_ - 1, start_.y_ - param);
-        array[size_ - 1].position.y = start_.y_ - param;
+        points_[size_ - 1] = Point (start_.x_ + size_ - 1, start_.y_ - param / coeff);
+        (*array)[size_ - 1].position.y = start_.y_ - param / coeff;
     }
 
     Math_vector ordinate (0, ordinate_len_, sf::Color::White);
@@ -75,6 +79,6 @@ void Plot::draw (sf::RenderTarget &target, double param)
 
     ordinate.draw (coord_system, target);
     abscissa.draw (coord_system, target);
-    target.draw (array);
+    target.draw (*array);
     // coord_system.draw (target);
 }
--- graphic_structures/plot/plot.h
+++ graphic_structures/plot/plot.h
@@ -15,6 +15,7 @@ class Plot
     int size_ = 0;
     double abscissa_len_ = 0;
     double ordinate_len_ = 0;
+    double max_val_ = 0;
 
     Point *points_ = nullptr;
     Point &start_;
@@ -24,9 +25,10 @@ class Plot
     
     Coord_system coord_system;
     Color color_;
+    sf::VertexArray *array;
 
 public:
-    Plot (Point &start, int x_len, int y_len, Color color = Color (255, 0, 0, 255));
+    Plot (Point &start, int x_len, int y_len, double max_val, Color color = Color (255, 0, 0, 255));
     ~Plot ();
     void draw (sf::RenderTarget &target, double param);
 };
